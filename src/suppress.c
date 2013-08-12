48461baf012893efc3c71e2ff8e6e1ea628dc017
chrisdew <cmsdew@gmail.com>
2013-08-12 15:42:13 +0100

    added autogenerated CDT project

---
+++ src/suppress.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : suppress.c
+ Author      : Chris Dew
+ Version     :
+ Copyright   : (C) Thorcom Systems Ltd. 2013, All Rights Reserved
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World"); /* prints Hello World */
+	return EXIT_SUCCESS;
+}
